403a98718102c873e73174562e3298ed7a3d70c2
Kent <Kent.hsieh@104.com.tw>
2021-04-20 10:04:15 +0800

    update db conf

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL = parse_url('postgres://gnltduxjibywuo:50ca60dfa13b021f09ba6ab4143c48a52920c349439478127ae9bf028340512f@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5bpg914m5dqrd');
 return [
 
     /*
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,10 +35,10 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
+        'sqlite_testing' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('dusk.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
