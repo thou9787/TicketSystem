8a83ce289b51839c3559bbeee6a86fc8f7526885
Kent <Kent.hsieh@104.com.tw>
2021-04-21 16:52:18 +0800

    try add 1 feature test CI

---
+++ tests/Feature/AdminTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Carbon\Carbon;
+class AdminTest extends TestCase
+{
+    use DatabaseTransactions;
+    public function setUp(): void
+    {
+        # code...
+        Carbon::setTestNow('2021-03-23 14:00:00');
+        parent::setUp();
+    }
+
+    public function testShowAllTickets()
+    {
+        $users = User::factory(10)->create();
+        $tickets = Ticket::factory(10)->create();
+        $this->demoUserLoginIn();
+        $response = $this->get('/admin/tickets');
+
+        $response->assertViewHas('tickets');
+    }
+
+    public function testShowAllUsers()
+    {
+        $this->demoUserLoginIn();
+        $users = User::factory(10)->create();
+        
+        $response = $this->get('/admin/users');
+
+        $response->assertViewHas('users');
+    }
+
+    
+}
