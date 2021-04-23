eb75e191da4b3b087e4da4daae2d2bd9b4a8490a
Kent <Kent.hsieh@104.com.tw>
2021-04-23 14:56:13 +0800

    testcase upload

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,46 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected function demoAdminLoginIn()
+    {
+        $admin = User::create(
+            [
+                'name' => 'admin',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email' => 'admin@admin.com',
+            ]
+        );
+
+        $this->be($admin);
+    }
+
     protected function demoUserLoginIn()
     {
-        $user = User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => 'password',
-            'role' => 'admin',
-        ]);
+        $user = User::create(
+            [
+                'name' => 'user',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'email' => '123@123.com'
+            ]
+        );
 
         $this->be($user);
+    }
+
+    protected function initMock($class)
+    {
+        $mock = \Mockery::mock($class);
+        $this->app->instance($class, $mock);
+
+        return $mock;
     }
 }
